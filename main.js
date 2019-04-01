f45740345e98b1aca4e97688785bb4b8de94536e
DoFeinstein <46566987+DoFeinstein@users.noreply.github.com>
2019-04-01 11:01:14 -0400

    changed circle.js

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
 function init() {
-
+alert("hello world");
+c.fillStyle= "red";
+c.fillRect(50,50,100,100);
     update();
 }
 
 function update() {
+
 
     requestAnimationFrame(update);
 }
