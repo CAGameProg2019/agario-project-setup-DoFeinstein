907d5c9bc021628112698c05242876489bfa06f9
DoFeinstein <46566987+DoFeinstein@users.noreply.github.com>
2019-04-02 21:08:21 -0400

    added functions

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height= window.innerHeight;
+
+let pos = new Vector (60,60);
+let vel = new Vector (.5, .2);
 function init() {
-alert("hello world");
-c.fillStyle= "red";
-c.fillRect(50,50,100,100);
+    console.log(pos.toString());
+
+
     update();
 }
 
 function update() {
+    c.clearRect(0,0,canvas.width, canvas.height);
 
+    pos.addVector(vel);
+
+    c.beginPath();
+    c.arc(pos.x,pos.y, 50, Math.PI * 2, false);
+    c.closePath();
+    c.stroke();
 
     requestAnimationFrame(update);
 }
@@ -17,3 +29,4 @@ function update() {
 window.addEventListener('load', function(event) {
     init();
 });
+;
--- vector.js
+++ vector.js
@@ -1,7 +1,29 @@
 class Vector {
-    
-    constructor() {
 
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    addVector(vec){
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    subVector(vec){
+        this.x -= vec.x;
+        this.y -= vec.y;
+        
+    }
+    scale(s){
+        this.x *= s;
+        this.y *= s ;
+    }
+
+    toString(){
+        return '<'+this.x+', ' + this.y+'>';
     }
 
 }
